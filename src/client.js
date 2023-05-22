a7bcde61dc205173fd7480df115c323caaa1c882
riya40 <priyanunna20@gmail.com>
2023-05-22 16:08:10 +0530

    Added:Client file

---
+++ src/client.js
@@ -0,0 +1,8 @@
+const {ApolloClient, InMemoryCache } = require("@apollo/client");
+
+const client = new ApolloClient({
+    uri:"http://localhost:4000",
+    cache: new InMemoryCache(),
+});
+
+export default client;
